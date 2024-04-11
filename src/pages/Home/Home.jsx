5fedd172c57bccb627ec4a83b817e292e9734176
jaheda8256 <jahedasultana47@gmail.com>
2024-04-11 23:18:36 +0600

    added three slider

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
+import Slider from "../Shared/Slider/Slider";
 
 
 const Home = () => {
     return (
         <div>
+            <Header></Header>
+            <Navbar></Navbar>
             <h2 className="text-3xl font-poppins font-bold">This is home</h2>
+            <div>
+                <Slider></Slider>
+            </div>
         </div>
     );
 };
